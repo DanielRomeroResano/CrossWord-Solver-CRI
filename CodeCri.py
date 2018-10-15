8d37107f840e12908cf6cd44946da8fac4cc342e
enriquefemo <enriquefemo@gmail.com>
2018-10-15 19:53:02 +0200

    forward acabadisimo tt

--- CodeCri.py
+++ CodeCri.py
@@ -28,7 +28,7 @@ def print_panel(panel):
     for i in range(0,len(panel)):
         for j in range(0,len(panel[i])):
             sys.stdout.write(panel[i][j])
- 
+
 
 def PanelFinal(tablaID,lva,mapa):
     tablaID[:,0] = tablaID[:,0]-1
@@ -48,20 +48,20 @@ def PanelFinal(tablaID,lva,mapa):
 
 
 def print_panel_final(panel):
-    print('\n'.join(['\t'.join(['{:4}'.format(item) for item in row]) 
+    print('\n'.join(['\t'.join(['{:4}'.format(item) for item in row])
       for row in panel]))
-           
-            
+
+
 def StringToInt8(panel_raw):
     linea = []
     panel = []
     for j in range(0,(len(panel_raw))):
         linea = panel_raw[j].split()
-        linea = np.array(linea,dtype=(np.unicode_,16)) 
-        linea[linea=="#"] = "-1"                        
+        linea = np.array(linea,dtype=(np.unicode_,16))
+        linea[linea=="#"] = "-1"
         panel.append(linea)
-    panel = np.array(panel,dtype=np.int8)            
-    return panel            
+    panel = np.array(panel,dtype=np.int8)
+    return panel
 
 
 def Int8ToString(panel_raw):
@@ -69,16 +69,16 @@ def Int8ToString(panel_raw):
     panel = []
     for j in range(0,(len(panel_raw))):
         linea = panel_raw[j]
-        linea[linea==-1] = 35        
+        linea[linea==-1] = 35
         linea=linea.tostring().decode("ascii")
         linea = list(linea)
-        panel.append(linea)           
-    return panel 
-            
+        panel.append(linea)
+    return panel
+
 
 def inicializarLVNA_LVA(tablaID):
-    LVNA = np.array([],dtype=np.int8) 
-    LVA = {} 
+    LVNA = np.array([],dtype=np.int8)
+    LVA = {}
     for ID in tablaID[:,0]:
         LVA[ID] = np.zeros(tablaID[ID-1,4],dtype=np.int8)
         LVA[ID] = np.reshape(LVA[ID], (-1,tablaID[ID-1,4]))
@@ -88,10 +88,14 @@ def inicializarLVNA_LVA(tablaID):
         LVNA = np.append(LVNA, lineaa)
         LVNA = np.reshape(LVNA, (-1,2))
     LVNA[::-1] = LVNA[LVNA[:,1].argsort(kind='quicksort')]
-    return LVNA,LVA            
+    return LVNA,LVA
 
 
-
+def create_da(tabla_ids,dicci):
+    da={}
+    for elem in tabla_ids:
+        da[elem[0]]=dicci[elem[4]]
+    return da
 
 
 
@@ -122,7 +126,7 @@ def busca_izq(panel,i,j):
     while (auxJ >= 0 and panel[i][auxJ] != -1):
         if(panel[i][auxJ] > 0):
             ret=1
-        auxJ = auxJ-1       
+        auxJ = auxJ-1
     return ret
 
 
@@ -135,7 +139,7 @@ def es_vertical(panel,id):
         if(busca_izq(panel,auxI,auxJ) == 1):
             ret = 1
     return ret
-        
+
 
 def busca_arriba(panel,i,j):
     ret=0
@@ -157,7 +161,7 @@ def es_horizontal(panel,id):
             ret = 1
         if(ret == 0 and es_vertical(panel,id)==0):
             ret = 2
-    return ret      
+    return ret
 
 
 def len_palabra(panel,id):
@@ -172,6 +176,7 @@ def len_palabra(panel,id):
             longitud = longitud +1
             i = i+1
     return longitud
+
 
 
 #0 es vertical, 1 horizontal y 2 ambos
@@ -217,11 +222,13 @@ def tabla_ids(panel):
 def classificarDiccionario(path):
     diccionari = {}
     for i in range(2,18):
-        diccionari[i] = np.array([],dtype=np.int8)  
+        diccionari[i] = np.array([],dtype=np.int8)
     for linea in open(path):
         palabra = []
-        aux = linea.strip('\t')
-        aux = linea.strip('\n')
+        aux = linea.replace('\r','')
+        aux = aux.replace('\t','')
+        #aux = aux.strip('\r')
+        aux = aux.replace('\n','')
         tamany = len(aux)
         for code in bytearray(aux):
             palabra.append(code)
@@ -236,7 +243,7 @@ def buscarChoque(panel,tablaID):
     diccChoques = {}
     tablaID[:,0] = tablaID[:,0]-1
     for ID in tablaID[:,0]:
-        diccChoques[ID+1] = np.array([],dtype=np.int8) 
+        diccChoques[ID+1] = np.array([],dtype=np.int8)
         posI = tablaID[ID,1]
         posJ = tablaID[ID,2]
         tamany = tablaID[ID,4]
@@ -251,7 +258,7 @@ def buscarChoque(panel,tablaID):
                             for pos2 in range(posJ2,posJ2+tamany2):
                                 if posI2==pos:
                                     if pos2==posJ:
-                                        array = np.array([pos-posI,ID2+1,pos2-posJ2],dtype=np.int8)  
+                                        array = np.array([pos-posI,ID2+1,pos2-posJ2],dtype=np.int8)
                                         diccChoques[ID+1] = np.append(diccChoques[ID+1], array)
                                         diccChoques[ID+1] = np.reshape(diccChoques[ID+1], (-1,3))
         else:
@@ -265,9 +272,9 @@ def buscarChoque(panel,tablaID):
                             for pos2 in range(posI2,posI2+tamany2):
                                 if posJ2==pos:
                                     if pos2==posI:
-                                        array = np.array([pos-posJ,ID2+1,pos2-posI2],dtype=np.int8)  
+                                        array = np.array([pos-posJ,ID2+1,pos2-posI2],dtype=np.int8)
                                         diccChoques[ID+1] = np.append(diccChoques[ID+1], array)
-                                        diccChoques[ID+1] = np.reshape(diccChoques[ID+1], (-1,3)) 
+                                        diccChoques[ID+1] = np.reshape(diccChoques[ID+1], (-1,3))
     tablaID[:,0] = tablaID[:,0]+1
     return diccChoques
 
@@ -288,7 +295,7 @@ def buscarChoque(panel,tablaID):
 ###############################################################################
 def Domini(palabra,d):
     return d[len(palabra[0])]
-    
+
 
 def SatisfaRestriccions(palabra,lva,pos,diccChoque):
     li = diccChoque[pos]
@@ -298,8 +305,8 @@ def SatisfaRestriccions(palabra,lva,pos,diccChoque):
                 if palabra[li[p,0]] != lva[pos][0,li[p,0]]:
                     return False
     return True
-    
-    
+
+
 def Insertar(palabra,lva, lvna,pos,diccChoque):
     li = diccChoque[pos]
     lva[pos] = palabra
@@ -315,7 +322,7 @@ def Insertar(palabra,lva, lvna,pos,diccChoque):
 #lvna columna 1 id, columna 2 choques
 def Backtracking(lva, lvna, d, diccChoque,r):
     if lvna.size == 0 or r == 1:
-        r=1 
+        r=1
         return lva,r
     var=lvna[0]
     for i in Domini(lva[var[0]],d):
@@ -352,35 +359,33 @@ def DominiFW(palabra,d):
 def ActualizarDominio(palabra,lva,pos,da, diccChoque, tablaID):
     #seCumple = True
     listaChoque = diccChoque[pos[0]]
-     
-    nouDiccionari = {}
-    
-    #print "listachoque", listaChoque
+    actualiza = True
     for j in range(0,listaChoque.shape[0]):
-        
-         
         id_palabra2 = listaChoque[j][1]
         corte_palabra2 = listaChoque[j][2]
         tamano = tablaID[id_palabra2-1][4]
-        nouDiccionari[tamano] = np.array([],dtype=np.int8) 
-        listaDicc = da[tamano]
+        nouDiccionari = np.array([],dtype=np.int8)
+        listaDicc = da[id_palabra2]
         for i in listaDicc :
-            if palabra[listaChoque[j][0]] != i[corte_palabra2]:
-                nouDiccionari[tamano] = np.append(nouDiccionari[tamano], i)
-                
-    #nouDiccionari = np.array(nouDiccionari,dtype=np.int8)
-    return nouDiccionari
+            if palabra[listaChoque[j][0]] == i[corte_palabra2]:
+                nouDiccionari = np.append(nouDiccionari, i)    
+        if nouDiccionari.size == 0:
+            actualiza = False
+            return da, actualiza
+        nouDiccionari = np.reshape(nouDiccionari, (-1,tamano))
+        da[id_palabra2] = nouDiccionari
+    return da, actualiza
 
 
 def ForwardBacktracking(lva, lvna, da, diccChoque,r, tablaID):
     if lvna.size == 0 or r == 1:
-        r=1 
+        r=1
         return lva,r
     var=lvna[0]
-    for i in DominiFW(lva[var[0]],da):
+    for i in da[var[0]]:
         #da = {}
-        da = ActualizarDominio(i, lva, var, da, diccChoque,tablaID)
-        if SatisfaRestriccions(i,lva,var[0],diccChoque) and da:
+        da,actualiza = ActualizarDominio(i, lva, var, da, diccChoque,tablaID)
+        if SatisfaRestriccions(i,lva,var[0],diccChoque) and actualiza:
                 if var[0] == lvna[0,0]:
                     lvna = np.delete(lvna,0,0)
                 lva,r=ForwardBacktracking(Insertar(i,lva, lvna,var[0],diccChoque),lvna,da, diccChoque,r,tablaID)
@@ -406,7 +411,7 @@ def ForwardBacktracking(lva, lvna, da, diccChoque,r, tablaID):
 """
 ###############################################################################
 if __name__ == '__main__':
-    
+
     ### INICIALITZACIO ###
     temps_ini_i = time.time()
     crossword = "crossword_CB.txt"
@@ -418,14 +423,15 @@ if __name__ == '__main__':
     tablaID = np.array(tablaID,dtype=np.int8)
     diccChoque = buscarChoque(panel,tablaID)
     LVNA,LVA = inicializarLVNA_LVA(tablaID)
-    temps_ini_f = time.time() 
+    da=create_da(tablaID,dicci)
+    temps_ini_f = time.time()
     temps_ini = temps_ini_f - temps_ini_i
-    
-    
+
+
     ### PRINTS ###
     print crossword
     print diccionari
-    print 
+    print
     print_panel(panel_raw)
     print
     print
@@ -433,25 +439,22 @@ if __name__ == '__main__':
     print("  id#posI#posJ#esHoriz(0=si,1=no)#tamaño#")
     print tablaID
     print
-    
+
+
     ### PROCESS ###
     ## BACKTRACKING ##
     r=0
     temps_Backtracking_i = time.time()
-    #LVA,r= Backtracking(LVA,LVNA,dicci,diccChoque,r)
-    LVA,r= ForwardBacktracking(LVA,LVNA,dicci,diccChoque,r,tablaID)
-    temps_Backtracking_f = time.time() 
+    LVA,r= Backtracking(LVA,LVNA,dicci,diccChoque,r)
+    #LVA,r= ForwardBacktracking(LVA,LVNA,da,diccChoque,r,tablaID)
+    temps_Backtracking_f = time.time()
     temps_Backtracking = temps_Backtracking_f - temps_Backtracking_i
-    
-    
-    
-    
-    
+
+
     ### FINAL ###
     panel = PanelFinal(tablaID,LVA,panel)
     panel = Int8ToString(panel)
     print_panel_final(panel)
-    print
     print
     print "\nInicialitzacio: %f segons." % (temps_ini)
     print "\nBacktracking: %f segons." % (temps_Backtracking)
